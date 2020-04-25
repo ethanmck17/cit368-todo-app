3de092430823989c5702c047e3334b2cc36f565a
ethanmck17 <34665606+ethanmck17@users.noreply.github.com>
2020-04-24 22:31:39 -0400

    Create Hashing.js

---
+++ part-2/Hashing.js
@@ -0,0 +1,21 @@
+//might use this or might edit create script with 
+var bcrypt = require('bcryptjs');
+//Instead of "B4c0/\/" a password is passed to the function for hashing and salting
+bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash("B4c0/\/", salt, function(err, hash) {
+        // Store hash in your password DB.
+        //
+    });
+});
+
+//other  option in PHP
+//passes $password and hashes it with bcrypt, the array is machine performance cost, 10 is the default and is low expense
+//$hash = password_hash($password, PASSWORD_BCRYPT, array('cost'=>11));
+
+//This set verifies the password
+/*if (password_verify($password, $hash)) {
+	if(password_needs_rehash($hash, PASSWORD_DEFAULT, $options)) {
+		$newHash = password_hash($password, PASSWORD_DEFAULT, $options);
+	}
+}
+*\
